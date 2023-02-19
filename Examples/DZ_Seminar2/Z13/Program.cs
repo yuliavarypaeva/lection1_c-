857546d56683204d1e6cc721f72e21b3cdd3d78e
Yulia <sid.dreamer@mail.ru>
2023-02-19 17:26:22 +0400

    домашняя работа

---
+++ Examples/DZ_Seminar2/Z13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число:"); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+string numberS = Math.Abs(number).ToString(); 
+
+if(numberS.Length > 2)
+ { 
+ Console.WriteLine($"Третья цифра {numberS[2]}"); 
+} 
+else 
+{ 
+ Console.WriteLine("Третьей цифры нет"); 
+}
