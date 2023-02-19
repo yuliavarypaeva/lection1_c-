aa58adefb3defcf9dd9dc9523b1e96d7022092a7
Yulia <sid.dreamer@mail.ru>
2023-02-19 19:30:01 +0400

    тернарная операция

---
+++ Examples/DZ_Seminar2/Z15_2.0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int numberDayOfWeek = int.Parse(Console.ReadLine());
+
+Console.WriteLine(numberDayOfWeek == 6 || numberDayOfWeek == 7 ? "да, это выходной" : "Нет");
