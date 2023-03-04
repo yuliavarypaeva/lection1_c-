8bfb4343b924af4d2d5a96504dcd9bedeb605faa
Yulia <sid.dreamer@mail.ru>
2023-03-04 23:22:20 +0400

    домашняя работа

---
+++ Examples/DZ_Seminar6/Z41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите колличество чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine()!);
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Количество чисел больше 0: {count}");
