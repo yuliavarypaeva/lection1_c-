51c4b7916edd9e25cd579c61157beb4f5a24c316
Yulia <sid.dreamer@mail.ru>
2023-03-17 19:53:13 +0400

    домашняя работа

---
+++ Examples/DZ_Seminar9/64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значения M и N.
+//  Напишите программу, которая выведет все чётные натуральные 
+//  числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> 2, 4
+// M = 4; N = 8 -> 4, 6, 8
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void NaturalEvenToLow(int m, int n)
+{
+ if (m > n)
+ return;
+ if (m % 2 == 0)
+ {
+  Console.Write($"{m}");
+  Console.Write(m >= n - 1 ? "." : ", ");
+ }
+NaturalEvenToLow(m + 1,n);
+
+}
+NaturalEvenToLow(m,n);
---
+++ Examples/DZ_Seminar9/68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Задайте значения M и N. Напишите программу,
+//  которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+// M = 28; N = 7 -> 7
+
+
+Console.Write("Введите значение M: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int gcd(int a, int b)
+{
+    while (b != 0)
+        b = a % (a = b);
+    return a;
+}
+Console.Write($"{}");
